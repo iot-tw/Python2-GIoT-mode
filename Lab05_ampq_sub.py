795f538dd9c7b39c937ac64ef6ce85a17c724944
Marty Chao <marty@browan.com>
2017-01-09 17:40:21 +0800

    add nodered chart

---
+++ Lab05_ampq_sub.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# revceive.py
+import pika
+credentials = pika.PlainCredentials('cloudservice','9Hxp5xi7')
+connection = pika.BlockingConnection(pika.ConnectionParameters(
+        host='52.196.119.165',
+        port=5671,
+        virtual_host='cloudservice',
+        credentials=credentials,
+        ssl=True))
+channel = connection.channel()
+
+channel.queue_declare(queue='hello')
+
+print(' [*] Waiting for messages. To exit press CTRL+C')
+
+def callback(ch, method, properties, body):
+    print " [x] Received %r" % (body,)
+
+channel.basic_consume(callback,
+                      queue='df.cloudservice.uldata',
+                      no_ack=True)
+
+channel.start_consuming()
