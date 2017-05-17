26234fe1c2d66a60e5ddc59f8e1d9d5de6392445
Marty Chao <marty@browan.com>
2017-05-17 11:12:45 +0800

    change help message

--- pub_dl_local.py
+++ pub_dl_local.py
@@ -15,26 +15,21 @@ from optparse import OptionParser
 import time
 now_time = time.strftime("%Hc%Mc%S")
 usage = "usage: %prog [options] [data]\n \
-    options: -d for sending hex data\n \
-             -c [A|a|C|c] for Class Mode.  Default is Class A\n \
-                Lowercase is unconfimed message,Uppercase is confirmed
-             -i IP for which MQTT Broker. Default is localhost\n \
-             -g GID for DL GW. Default is 00001c497b431fcd\n \
-             -m MAC for DL Node. Default is 04000476 \n \
     e.g.: '%prog --data \"12345678901\" will downlink to module 04000476 by Localhost Broker,Class A confirmed"
 parser = OptionParser(usage)
 parser.add_option("-d", "--data", action="store", dest="data",
                   default=now_time,
-                  help="sending data")
+                  help="for sending HEX data")
 parser.add_option("-c", "--class", action="store", dest="classtype",
                   default="A",
-                  help="sending data")
+                  help="[A|a|C|c] for Class Mode.  Default is Class A\n \
+                Lowercase is unconfimed message,Uppercase is confirmed\n")
 parser.add_option("-i", "--ip", action="store", dest="host",
                   default="localhost",
-                  help="setting Broker IP")
+                  help="IP for which MQTT Broker. Default is localhost")
 parser.add_option("-g", "--gid", action="store", dest="GID",
                   default="00001c497b431fcd",
-                  help="setting ODU/IDU GID")
+                  help="GID for DL GW. Default is 00001c497b431fcd")
 parser.add_option("-m", "--mac", action="store", dest="MAC",
                   default="04000476",
                   help="setting DL target Moudle MAC")
